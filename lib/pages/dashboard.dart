e5fa2ef4a9ad46493978be3e870a9764699276d6
Chance Snow <git@chancesnow.me>
2024-06-05 20:42:15 -0500

    Add an `AppBar` to the dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -23,8 +23,60 @@ class _DashboardPageState extends State<DashboardPage> {
     final config = AppConfig.of(context);
 
     return Scaffold(
+      appBar: AppBar(
+        // FIXME: backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Monday January 1, 1900',
+          // TODO: formatDate(_model.day!),
+          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+            fontFamily: 'Outfit',
+            color: Colors.white,
+            fontSize: 22.0,
+            letterSpacing: 0.0,
+          ),
+        ),
+        actions: [
+          Tooltip(
+            message: 'Today',
+            waitDuration: Duration(milliseconds: 100),
+            showDuration: Duration(milliseconds: 1500),
+            triggerMode: TooltipTriggerMode.tap,
+            child: IconButton(
+              icon: Icon(
+                Icons.today,
+                // FIXME: This should use a color from `ThemeData`
+                color: Colors.white,
+                size: 24.0,
+              ),
+              onPressed: () {
+                // TODO: Date-range calendar chooser
+              },
+            ),
+          ),
+          Tooltip(
+            message: 'More options...',
+            waitDuration: Duration(milliseconds: 100),
+            showDuration: Duration(milliseconds: 1500),
+            triggerMode: TooltipTriggerMode.tap,
+            child: IconButton(
+              icon: Icon(
+                Icons.more_vert,
+                // FIXME: This should use a color from `ThemeData`
+                color: Colors.white,
+                size: 24.0,
+              ),
+              onPressed: () async {
+                // TODO: Open action menu
+              },
+            ),
+          ),
+        ],
+        centerTitle: false,
+        elevation: 2.0,
+      ),
       body: Container(
-        // color: ChoresTheme.biceBlue,
+        // FIXME: color: ChoresTheme.biceBlue,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [ChoresTheme.biceBlue, ChoresTheme.amethyst],
@@ -49,7 +101,7 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Create a New Chore',
         child: Icon(Icons.add),
       ),
     );
