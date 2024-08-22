4376ac26f6f4f03f984826ea2b8c33c2ce18d68a
Chance Snow <git@chancesnow.me>
2024-08-22 15:08:30 -0500

    Simplify layout constraints, Remove dead code

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,7 +17,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int _counter = 0;
   var _emailController = TextEditingController();
   late FocusNode _emailFocusNode;
   var _passwordController = TextEditingController();
@@ -39,10 +38,6 @@ class _LoginPageState extends State<LoginPage> {
     _passwordFocusNode.dispose();
 
     super.dispose();
-  }
-
-  void _incrementCounter() {
-    setState(() => _counter++);
   }
 
   @override
@@ -86,210 +81,184 @@ class _LoginPageState extends State<LoginPage> {
     );
     return Scaffold(
       body: Container(
-        // FIXME: color: ChoresTheme.biceBlue,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [ChoresTheme.biceBlue, ChoresTheme.amethyst],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0, 0.9],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(16.0, 16.0, 16.0, 44.0),
-                child: Container(
-                  width: double.infinity,
-                  constraints: BoxConstraints(
-                    maxWidth: 570.0,
+        constraints: BoxConstraints.expand(),
+        padding: EdgeInsetsDirectional.fromSTEB(16.0, 16.0, 16.0, 44.0),
+        decoration: BoxDecoration(color: ChoresTheme.biceBlue),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(maxWidth: 570.0),
+              padding: EdgeInsets.all(24.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 4.0,
+                    color: Color(0x33000000),
+                    offset: Offset(0.0, 2.0),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Cleanup Your Act',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .typography
+                        .white
+                        .headlineMedium
+                        ?.apply(fontFamily: 'Outfit')
+                        .copyWith(letterSpacing: 0.0),
                   ),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).canvasColor,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 4.0,
-                        color: Color(0x33000000),
-                        offset: Offset(0.0, 2.0),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(12.0),
+                  Text(
+                    'Login to track your household chores.',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .typography
+                        .white
+                        .labelLarge
+                        ?.apply(fontFamily: 'Readex Pro')
+                        .copyWith(letterSpacing: 0.0),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(24.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Cleanup Your Act',
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .typography
-                              .white
-                              .headlineMedium
-                              ?.apply(fontFamily: 'Outfit')
-                              .copyWith(letterSpacing: 0.0),
+                  TextField(
+                    controller: _emailController,
+                    focusNode: _emailFocusNode,
+                    autofocus: true,
+                    autofillHints: [AutofillHints.email],
+                    obscureText: false,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    style:
+                        Theme.of(context).typography.white.bodyLarge?.copyWith(letterSpacing: 0.0),
+                    keyboardType: TextInputType.emailAddress,
+                    // FIXME: validator: _emailTextControllerValidator.asValidator(context),
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    focusNode: _passwordFocusNode,
+                    autofocus: true,
+                    autofillHints: [AutofillHints.password],
+                    obscureText: !_passwordVisibility,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: InkWell(
+                        onTap: () => setState(() => _passwordVisibility = !_passwordVisibility),
+                        focusNode: FocusNode(skipTraversal: true),
+                        child: Icon(
+                          _passwordVisibility
+                              ? Icons.visibility_outlined
+                              : Icons.visibility_off_outlined,
+                          color: Theme.of(context).typography.white.labelLarge?.color,
+                          size: 24.0,
                         ),
-                        Text(
-                          'Login to track your household chores.',
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .typography
-                              .white
-                              .labelLarge
-                              ?.apply(fontFamily: 'Readex Pro')
-                              .copyWith(letterSpacing: 0.0),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          child: TextField(
-                            controller: _emailController,
-                            focusNode: _emailFocusNode,
-                            autofocus: true,
-                            autofillHints: [AutofillHints.email],
-                            obscureText: false,
-                            decoration: InputDecoration(labelText: 'Email'),
-                            style: Theme.of(context)
-                                .typography
-                                .white
-                                .bodyLarge
-                                ?.copyWith(letterSpacing: 0.0),
-                            keyboardType: TextInputType.emailAddress,
-                            // FIXME: validator: _emailTextControllerValidator.asValidator(context),
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          child: TextField(
-                            controller: _passwordController,
-                            focusNode: _passwordFocusNode,
-                            autofocus: true,
-                            autofillHints: [AutofillHints.password],
-                            obscureText: !_passwordVisibility,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              suffixIcon: InkWell(
-                                onTap: () =>
-                                    setState(() => _passwordVisibility = !_passwordVisibility),
-                                focusNode: FocusNode(skipTraversal: true),
-                                child: Icon(
-                                  _passwordVisibility
-                                      ? Icons.visibility_outlined
-                                      : Icons.visibility_off_outlined,
-                                  color: Theme.of(context).typography.white.labelLarge?.color,
-                                  size: 24.0,
-                                ),
-                              ),
-                            ),
-                            style: Theme.of(context)
-                                .typography
-                                .white
-                                .bodyLarge
-                                ?.copyWith(letterSpacing: 0.0),
-                            // FIXME: validator: _passwordTextControllerValidator.asValidator(context),
-                          ),
-                        ),
-                        InkWell(
-                          splashColor: Colors.transparent,
-                          focusColor: Colors.transparent,
-                          hoverColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          onTap: () async {
-                            context.go("/resetPassword");
-                            context.pushNamed(
-                              'ResetPassword',
-                              extra: <String, dynamic>{
-                                kTransitionInfo: PageTransitionType.rightToLeft,
-                              },
-                            );
-                          },
-                          child: Text('Reset Password', style: ChoresTheme.of(context).hyperlink),
-                        ),
-                        FilledButton(
-                          onPressed: () async {
-                            setState(() => _loggingIn = true);
-                            final user = (await Supabase.instance.client.auth.signInWithPassword(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                            ))
-                                .user;
-                            if (user == null) return;
-                            context.pushReplacementNamed('/');
-                          },
-                          child: Text('Log In'),
-                        ),
-                        orDivider,
-                        // See https://console.cloud.google.com/apis/credentials?authuser=1&project=chore-chart-fo3w11
-                        FilledButton.icon(
-                          onPressed: () async {
-                            setState(() => _loggingIn = true);
-                            await Supabase.instance.client.auth.signInWithOAuth(
-                              OAuthProvider.google,
-                              redirectTo: '',
-                              authScreenLaunchMode: LaunchMode.inAppBrowserView,
-                            );
-
-                            final user = await Supabase.instance.client.auth.onAuthStateChange
-                                .lastWhere(
-                                  (state) => state.event == AuthChangeEvent.signedIn,
-                                  orElse: () => AuthState(AuthChangeEvent.initialSession, null),
-                                )
-                                .then((state) => state.session?.user);
-                            if (user != null) context.pushReplacementNamed('/');
-                          },
-                          icon: FaIcon(
-                            FontAwesomeIcons.google,
-                            size: 20,
-                          ),
-                          label: Text('Login with Google'),
-                        ),
-                        FilledButton(
-                          onPressed: () async {
-                            setState(() => _loggingIn = true);
-                            try {
-                              // FIXME: https://supabase.com/docs/guides/auth/auth-anonymous
-                              final user = await Supabase.instance.client.auth
-                                  .signInAnonymously()
-                                  .then((response) => response.user);
-                              if (user != null)
-                                context.pushReplacementNamed("/");
-                              else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      'Could not login anonymously. Please try again or create an account.',
-                                    ),
-                                    duration: Duration(milliseconds: 4000),
-                                  ),
-                                );
-                              }
-                            } finally {
-                              setState(() => _loggingIn = false);
-                            }
-                          },
-                          child: Text('Continue as Guest'),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.only(top: 8.0),
-                          child: buildSignUp(context),
-                        ),
-                      ].divide(SizedBox(height: 16.0)),
+                      ),
+                    ),
+                    style:
+                        Theme.of(context).typography.white.bodyLarge?.copyWith(letterSpacing: 0.0),
+                    // FIXME: validator: _passwordTextControllerValidator.asValidator(context),
+                  ),
+                  InkWell(
+                    splashColor: Colors.transparent,
+                    focusColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    onTap: () async {
+                      context.go("/resetPassword");
+                      context.pushNamed(
+                        'ResetPassword',
+                        extra: <String, dynamic>{kTransitionInfo: PageTransitionType.rightToLeft},
+                      );
+                    },
+                    child: Text(
+                      'Reset Password',
+                      style: ChoresTheme.of(context).hyperlink,
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
-                // FIXME: Container.animateOnPageLoad(),
+                  FilledButton(
+                    onPressed: () async {
+                      setState(() => _loggingIn = true);
+                      final user = (await Supabase.instance.client.auth.signInWithPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      ))
+                          .user;
+                      if (user == null) return;
+                      context.pushReplacementNamed('/');
+                    },
+                    child: Text('Log In'),
+                  ),
+                  orDivider,
+                  // See https://console.cloud.google.com/apis/credentials?authuser=1&project=chore-chart-fo3w11
+                  FilledButton.icon(
+                    onPressed: () async {
+                      setState(() => _loggingIn = true);
+                      await Supabase.instance.client.auth.signInWithOAuth(
+                        OAuthProvider.google,
+                        redirectTo: '',
+                        authScreenLaunchMode: LaunchMode.inAppBrowserView,
+                      );
+
+                      final user = await Supabase.instance.client.auth.onAuthStateChange
+                          .lastWhere(
+                            (state) => state.event == AuthChangeEvent.signedIn,
+                            orElse: () => AuthState(AuthChangeEvent.initialSession, null),
+                          )
+                          .then((state) => state.session?.user);
+                      if (user != null) context.pushReplacementNamed('/');
+                    },
+                    icon: FaIcon(
+                      FontAwesomeIcons.google,
+                      size: 20,
+                    ),
+                    label: Text('Login with Google'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      setState(() => _loggingIn = true);
+                      try {
+                        // FIXME: https://supabase.com/docs/guides/auth/auth-anonymous
+                        final user = await Supabase.instance.client.auth
+                            .signInAnonymously()
+                            .then((response) => response.user);
+                        if (user != null)
+                          context.pushReplacementNamed("/");
+                        else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                'Could not login anonymously. Please try again or create an account.',
+                              ),
+                              duration: Duration(milliseconds: 4000),
+                            ),
+                          );
+                        }
+                      } finally {
+                        setState(() => _loggingIn = false);
+                      }
+                    },
+                    child: Text('Continue as Guest'),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(top: 8.0),
+                    child: buildSignUp(context),
+                  ),
+                ].divide(SizedBox(height: 16.0)),
               ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text('Copyright © 2024 Snow Developments, LLC.'),
-              ),
-            ],
-          ),
+            ),
+            // FIXME: Container.animateOnPageLoad(),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Text('Copyright © 2024 Snow Developments, LLC.'),
+            ),
+          ],
         ),
       ),
     );
@@ -298,6 +267,7 @@ class _LoginPageState extends State<LoginPage> {
   RichText buildSignUp(BuildContext context) {
     return RichText(
       textScaler: MediaQuery.of(context).textScaler,
+      textAlign: TextAlign.center,
       text: TextSpan(
         children: [
           TextSpan(text: 'Don\'t have an account?  '),
@@ -310,13 +280,7 @@ class _LoginPageState extends State<LoginPage> {
                 // Go to Create Account page
                 context.pushNamed(
                   'CreateAccount',
-                  extra: <String, dynamic>{
-                    // FIXME: Use the PageTransitionsTheme
-                    // kTransitionInfoKey: TransitionInfo(
-                    //     hasTransition: true,
-                    //     transitionType:
-                    //         PageTransitionType.rightToLeft),
-                  },
+                  extra: <String, dynamic>{kTransitionInfo: PageTransitionType.rightToLeft},
                 );
               },
           )
